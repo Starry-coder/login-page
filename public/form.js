34c029be9a9bd54ea4c0b5e9caf799fa0666f223
unknown <aarryyaann1502051@gmail.com>
2023-10-03 22:17:02 +0530

    4

--- public/form.js
+++
@@ -1,9 +0,0 @@
-//laoding animation
-
-const form = [...document.querySelectorAll('.form').children];
-
-form.forEach((item, i) => {
-    setTimeout(() => {
-        item.style.opacity = 1;
-    }, i * 100);
-})
