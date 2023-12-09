cf2df1250b33815479b92c8137dd8351c1d666f9
unknown <aarryyaann1502051@gmail.com>
2023-12-10 02:54:39 +0530

    update

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,42 +1,26 @@
-const mongoose = require("mongoose")
-const url = "mongodb+srv://aryan:aryan@cluster0.c68jf98.mongodb.net/?retryWrites=true&w=majority"
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://aryan:aryan@cluster0.c68jf98.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-const logInSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-})
-
-const collection = mongoose.model("collection1", logInSchema)
-
-const connectToMongoDB = async () => {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-};
-
-module.exports = {collection, connectToMongoDB}
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
